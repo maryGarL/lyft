1dc7a9f2d66c630ea87c57d8efa45e00da6786af
Mariel Garcia <maryel_565@hotmail.com>
2017-05-02 18:21:45 -0500

    lyft 3

--- assets/js/main.js
+++ assets/js/main.js
@@ -2,17 +2,27 @@ var myNav=document.getElementById('navegador');
 var botoncito=document.getElementById("aparece");
 var boton1=document.getElementById("boton1");
 var items=document.getElementById("nav");
+var anchor1=document.getElementById("anchor1");
+var anchor2=document.getElementById("anchor2");
+var anchor3=document.getElementById("anchor3");
 window.onscroll=function () {
 "use strict";
 if (document.body.scrollTop >= 200 ) {
-       boton1.classList.add("pink-boton");
+
        boton1.classList.remove("white-boton");
-       botoncito.classList.add("pink-boton");
+       boton1.classList.add("pink-boton");
        botoncito.classList.remove("white-boton");
-       myNav.classList.add("new-nav");
+       botoncito.classList.add("pink-boton");
        myNav.classList.remove("antique-nav");
-       items.classList.add("pink-color");
-       items.classList.remove("white-color");
+       myNav.classList.add("new-nav");
+       botoncito.style.display="inline-block";
+       anchor1.classList.remove("white-color");
+       anchor1.classList.add("pink-color");
+       anchor2.classList.remove("white-color");
+       anchor2.classList.add("pink-color");
+       anchor3.classList.remove("white-color");
+       anchor3.classList.add("pink-color");
+
    }
    else {
        boton1.classList.add("white-boton");
@@ -21,7 +31,12 @@ if (document.body.scrollTop >= 200 ) {
        botoncito.classList.remove("pink-boton");
        myNav.classList.add("antique-nav");
        myNav.classList.remove("new-nav");
-       items.classList.remove("pink-color");
-       items.classList.add("white-color");
+       botoncito.style.display="none";
+       anchor1.classList.remove("pink-color");
+       anchor1.classList.add("white-color");
+       anchor2.classList.remove("pink-color");
+       anchor2.classList.add("white-color");
+       anchor3.classList.remove("pink-color");
+       anchor3.classList.add("white-color");
    }
 };
